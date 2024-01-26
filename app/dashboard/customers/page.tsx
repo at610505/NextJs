c8e16e5c7229b73ad57964c0cbb173810e416151
Anshuman Tiwari <118616967+iam-anshuman@users.noreply.github.com>
2024-01-26 13:49:12 +0530

    Done

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+export default function Page(){
+    return(<>
+        <h3>This is customers page</h3>
+    </>)
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+export default function Page(){
+    return(
+        <>
+            <h4>Invoices Page</h4>
+        </>
+    )
+}
